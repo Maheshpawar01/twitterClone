6aa6c369a816378cccb4f58174e24d028de2bba1
Mahesh Pawar <mp002995@gmail.com>
2024-12-16 21:38:08 +0530

    repush code

--- frontend/twitterclone/src/utils/constant.jsx
+++ frontend/twitterclone/src/utils/constant.jsx
@@ -1,8 +1,10 @@
 
 // export const backurl = "https://twitterclone-1vps.onrender.com"
-
+// https://twitterclone-backend-rrjg.onrender.com
 export const USER_API_END_POINT = `https://twitterclone-backend-rrjg.onrender.com/api/v1/user`;
 export const TWEET_API_END_POINT = `https://twitterclone-backend-rrjg.onrender.com/api/v1/tweet`;
+// export const USER_API_END_POINT = `http://localhost:8080/api/v1/user`;
+// export const TWEET_API_END_POINT = `http://localhost:8080/api/v1/tweet`;
 
 export const timeSince = (timestamp) => {
   let time = Date.parse(timestamp);
